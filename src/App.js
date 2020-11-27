684ce161e7fb5c9fa6e83f06ea53003a94d8ce65
Chung Kang <chung.kang@gmail.com>
2020-11-27 16:04:06 -0600

    Add mousetrap to listen for Konami code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Konami from './components/Konami';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <Konami></Konami>
       </header>
     </div>
   );
---
+++ src/components/Konami.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import Mousetrap from 'mousetrap';
+
+class Konami extends Component {
+    constructor() {
+    super();
+  }
+
+  componentDidMount() {
+    Mousetrap.bind([`1 2 3 4`, `up up down down left right left right b a`], this.popUp);
+  }
+
+  componentWillUnmount() {
+    Mousetrap.unbind([`1 2 3 4`, `up up down down left right left right b a`]);
+  }
+
+  popUp() {
+    alert('The Konami code happened! Wow!');
+  }
+
+  render() {
+    return (
+      <div>Hello, this is Konami!</div>
+    )
+  }
+}
+
+export default Konami;
